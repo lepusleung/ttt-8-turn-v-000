1fab2cafc7959aa55884ac5b46886d5352d213f3
Man Leung <lepusleung@gmail.com>
2017-11-17 19:59:33 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,13 @@ end
 def move(board, index, current_player = "X")
   board[index] = current_player
 end
+
+def valid_move?(board, index)
+  # if index.between?(1,9)
+  #   if !position_taken?(board, index)
+  #     true
+  #   end
+  # end
+
+  index.between?(0,8) && !position_taken?(board, index)
+end
